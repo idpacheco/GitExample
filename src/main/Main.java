c6ad7cef50eb47488f68f2c791112051d07f936c
Isis Dayan Pacheco Bravo <idpacheco@uninorte.edu.co>
2025-03-17 15:30:08 -0500

    Feat: Added Person Class

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,12 +4,15 @@
  */
 package main;
 
+import core.Person;
+
 /**
  *
- * @author isisp
+ * @author idpacheco
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello World!");
+        Person person = new Person(200210177, "Isis Pacheco", 18);
+        System.out.println(person);
     }
 }
